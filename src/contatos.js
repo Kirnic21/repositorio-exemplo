8cae6df67de768f8d44aadecc59be677d25dd9cc
Kirnic21 <marcelokenji12121@gmail.com>
2023-07-19 18:32:43 -0300

    adicionei header

---
+++ src/contatos.js
@@ -0,0 +1,11 @@
+const container = document.querySelector("#container")
+
+/*<div class = "hero">
+        <div class = "hero1-c">
+            <h1>Insira alguma coisa</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas itaque nesciunt similique eius dolores deleniti qui quidem hic quia cupiditate, minima ullam veritatis mollitia dolorum maxime magni! Saepe, inventore dolore!</p>
+        </div>
+        <div class = "imagem">
+            <img src = "imagem.jpg" id = "imagem">
+        </div>
+    </div>*/
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import "./style.css";
+const container = document.querySelector("#container")
